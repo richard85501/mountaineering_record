a7c29c875d2e3cf7d82d65e71d80e8b4e50cd6e4
richard85501 <liuchang85501@gmail.com>
2022-01-21 17:59:07 +0800

    [modify] 1.change nav menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,27 +16,27 @@ function NavBar() {
                 </div>
                 <div className='aboutNavMenu'>
                     <Link to="/about" className="aboutNavMenuUnit">
-                        <div className="aboutNavMenuUnitContent">adipisci(登山日誌)</div>
+                        <div className="aboutNavMenuUnitContent">登山日誌</div>
                         <div className='aboutNavMenuUnitHover'></div>
                     </Link>
                     <Link to="/Altitude" className="aboutNavMenuUnit">
-                        <div className="aboutNavMenuUnitContent">adipisci(海拔)</div>
+                        <div className="aboutNavMenuUnitContent">海拔</div>
                         <div className='aboutNavMenuUnitHover'></div>
                     </Link>
                     <Link to="/Equipment" className="aboutNavMenuUnit">
-                        <div className="aboutNavMenuUnitContent">adipisci(裝備)</div>
+                        <div className="aboutNavMenuUnitContent">裝備</div>
                         <div className='aboutNavMenuUnitHover'></div>
                     </Link>
                     <Link to ="/Itinerary" className="aboutNavMenuUnit">
-                        <div className="aboutNavMenuUnitContent">adipisci(行程)</div>
+                        <div className="aboutNavMenuUnitContent">行程</div>
                         <div className='aboutNavMenuUnitHover'></div>
                     </Link>
                     <Link to="/landscape" className="aboutNavMenuUnit">
-                        <div className="aboutNavMenuUnitContent">landscape(風景)</div>
+                        <div className="aboutNavMenuUnitContent">風景</div>
                         <div className='aboutNavMenuUnitHover'></div>
                     </Link>
                     <Link to="/Ingredients" className="aboutNavMenuUnit">
-                        <div className="aboutNavMenuUnitContent">adipisci(食材)</div>
+                        <div className="aboutNavMenuUnitContent">食材</div>
                         <div className='aboutNavMenuUnitHover'></div>
                     </Link>
                 </div>
